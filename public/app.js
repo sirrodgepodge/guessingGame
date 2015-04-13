764a91940c5779d0d28d0ec5f074aca1b3385bac
sirrodgepodge <rwcbeaman@gmail.com>
2015-04-13 20:39:48 +0000

    jQuery cray

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
-var answer = Math.ceil(Math.random()*100).toString();
+var answer = Math.ceil(Math.random()*100);
+var origBackground = $('body').css('background');
 
 var fortyTwo = function(){
     $('#guessbox').attr("placeholder","42");
@@ -8,44 +9,54 @@ var hint = function(){
     $('#guessbox').attr("placeholder", answer);
 };
 
-var guessNum = 0;
-
-var guess =  function(){
+var guessRem = 5;
+var guess =  function() {
     var theirGuess = $('#guess').val();
-
-    //add something flashing messages?
+    $('#guess').val() = "";
+    
+    //Check guess type
+    if(theirGuess.length === 0 || typeof theirGuess !== 'number') {
+	$('#guessbox').attr("placeholder", "Number Please!");
+	return;
+    }
     
     //Create guess list if first, end game if guess number 5 and wrong
-    if (theirGuess !== answer) {
-	if (guessNum === 0) {
-	    $('<ul class="guessList">Wrong Guesses:</ul>').appendTo(".main");
-	}
-	if (guessNum >= 5) {
-	    console.log("You lost bro");
-	    return;
-	}
+    if (theirGuess !== answer && guessRem === 1) {
+	console.log("You lost bro");
+	return;
     }
 
     //Increment guess number
-    guessNum += 1;
-    
-    if(theirGuess.length === 0 || typeof theirGuess !== "number") {
-	console.log("Need a value!");
-    } else if (theirGuess < answer) {
-	$('<li class="hot">'+theirGuess+'</li>').appendTo(".guessList");
-	console.log("too low");
+    guessRem -= 1;
+    $('#guessbox').attr("placeholder", guessRem + " guesses left!");
+
+    //Provide guess feedback
+    if (theirGuess < answer) {
+	$('<li class="below">'+theirGuess+'</li>').appendTo(".guessList");
+	$('#guessFBack').css('color', 'blue').text("Too Low!");
     } else if (theirGuess > answer) {
-	$('<li class="cold">'+theirGuess+'</li>').appendTo(".guessList");
-	console.log("too high");
+	$('<li class="above">'+theirGuess+'</li>').appendTo(".guessList");
+	$('#guessFBack').css('color', 'red').text("Too High!");
     } else {
-	console.log("You got it!!!!!");
+	console.log("You got it!!!!!");	
+	$('#guess').text("Play Again!");
+	$('#guess').attr('id', 'playAgain');
     }
+};
+
+var playAgain = function() {
+    answer = Math.ceil(Math.random()*100);
+    guessRem = 5;
+    $('#guessFBack').text("");
+    $('body').css('background', origBackground);
+    $('#playAgain').attr('id','guess');
 };
 
 var main= function(){
     $('#fortytwo').click(fortyTwo);
     $('#hint').click(hint);
     $('#guess').click(guess);
+    $('#playAgain').click(playAgain);
 }
 
 $(document).ready(main);
